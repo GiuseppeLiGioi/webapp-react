437a334a148dca5a2330a64973efa520d4d4640b
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-20 17:09:21 +0100

    add: GlobalContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import DefaultLayout from "./layouts/DefaultLayout"
 import {Routes, Route, BrowserRouter} from 'react-router-dom'
 import HomePage from "./pages/HomePage"
 import MoviesPage from "./pages/MoviesPage"
+import GlobalContext from "./contexts/GlobalContext"
+import { useState } from "react"
 function App() {
  
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
+    <GlobalContext.Provider value={{isLoading, setIsLoading}}>
     <BrowserRouter>
     <Routes>
       <Route Component={ DefaultLayout }>
       <Route path="/" Component={ HomePage } />
-      <Route path="/movies/:id" Component={ MoviesPage } />
-      
-
-   
+      <Route path="/movies/:id" Component={ MoviesPage } />       
       </Route>
-    </Routes>
-    
-    
+    </Routes>   
     </BrowserRouter>
+    </GlobalContext.Provider>
     
     </>
   )
