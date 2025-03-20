f98a19de072292154babaf1731980f0e1b0627a9
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-20 17:04:41 +0100

    add: loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,17 @@
+function Loader () {
+    return(
+        <>
+        <div className="overlay">
+            <div className="spinner-border text-primary"> 
+
+            </div>
+
+        </div>
+        
+        
+        </>
+
+    )
+}
+
+export default Loader;
