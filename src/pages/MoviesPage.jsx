4bae0f9eafbd14d72dcab6d9e16c9471207535be
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-20 17:32:08 +0100

    add: loader at book single page, fine es.

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -3,8 +3,12 @@ import {useState, useEffect} from "react"
 import {useParams} from 'react-router-dom'
 import ReviewCard from "../components/ReviewCard";
 import ReviewForm from "../components/ReviewForm"
+import GlobalContext from "../contexts/GlobalContext"
+import { useContext } from "react";
 
 function MoviesPage () {
+
+    const { setIsLoading } = useContext(GlobalContext);
 
     const {id} = useParams();
 
@@ -12,10 +16,12 @@ function MoviesPage () {
 
     const fetchMovie = () => {
         console.log("fetching movie...")
+        setIsLoading(true)
 
         axios.get(`http://localhost:3000/movies/${id}`)
         .then ((res) => setMovie(res.data))
         .catch((err) => console.error(err))
+        .then(() => setIsLoading(false))
     }
 
     useEffect(fetchMovie, [id]);
